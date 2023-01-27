c9f2b9b325fcb9b3f1c5c67ba3dc0fea23a80d23
Gurdeep <khalsagudeep@gmail.com>
2023-01-27 21:56:22 +0530

    fix
    fix positions of hand at continous intervals

--- 02 - JS and CSS Clock/app.js
+++ 02 - JS and CSS Clock/app.js
@@ -6,13 +6,16 @@ let setDate = () => {
 
   const seconds = present.getSeconds();
   const secondDeg = (seconds / 60) * 360 + 90;
-  secondHand.style.transform = `rotate(${secondDeg}deg)`;
-  const minDeg = secondDeg / 60 + 90;
-  minuteHand.style.transform = `rotate(${minDeg}deg)`;
+
+  const minute = present.getMinutes();
+  const minDeg = (minute / 60) * 360 + (seconds / 60) * 6 + 90;
+
   const Hours = present.getHours();
-  const hourDeg = (Hours / 12) * 360 + minDeg / 12 + 90;
+  const hourDeg = (Hours / 12) * 360 + (minute / 60) * 30 + 90;
+
+  secondHand.style.transform = `rotate(${secondDeg}deg)`;
+  minuteHand.style.transform = `rotate(${minDeg}deg)`;
   hourHand.style.transform = `rotate(${hourDeg}deg)`;
-  console.log(present.getHours(), present.getMinutes());
 };
 
 setInterval(setDate, 1000);
